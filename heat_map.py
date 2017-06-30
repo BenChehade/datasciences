a9d1887971024af7a1c90afaced242513a2dfb85
DataMonsterBoy <d.j.g.wyatt@gmail.com>
2017-06-30 17:01:14 +0100

    folder name change

--- heat_map.py
+++ heat_map.py
@@ -5,17 +5,28 @@ Created on Wed Jun 21 17:27:18 2017
 @author: DWyatt
 """
 import pandas as pd
+import numpy as np
 import seaborn as sns
+import matplotlib.pyplot as plt
 import sys
 
 df_train = pd.read_csv('train.csv')
+cols = df_train.columns
 target = 'SalePrice'
 variables = [column for column in df_train.columns if column!=target]
 
+k = 10
 corr = df_train.corr()
-sns_heat= sns.heatmap(corr, square=True)
+cols = corr.nlargest(k, 'SalePrice')['SalePrice'].index
+cm = np.corrcoef(df_train[cols].values.T)
+sns.set(font_scale=1.25)
+sns_heat = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values,
+                       xticklabels=cols.values)
+plt.yticks(rotation=0)
+plt.xticks(rotation='vertical')
+plt.tight_layout()
 fig = sns_heat.get_figure()
-fig.savefig('heat.png')
+fig.savefig('topTen - heatMap.png')
 print([target])
 print(variables)
 #sys.exit()
