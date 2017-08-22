225d22b2ebee4483ccaab01718cf89e66d1e037e
DataMonsterBoy <d.j.g.wyatt@gmail.com>
2017-08-22 16:14:34 +0100

    missing data

---
+++ code/cat_variables.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from missing_data import missing_list
+import yaml as yml
+
+df_train = pd.read_csv('train.csv')
+#with open('reference_file.yaml') as f:
+    #column_dict = yml.safe_load(f)
+
