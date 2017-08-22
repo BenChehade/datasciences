6305245693006e7692cf9ff47296ac0aa123cd43
DataMonsterBoy <d.j.g.wyatt@gmail.com>
2017-08-22 17:26:06 +0100

    missing data

--- code/cat_variables.py
+++ code/cat_variables.py
@@ -1,8 +1,23 @@
 import pandas as pd
-from missing_data import missing_list
 import yaml as yml
 
-df_train = pd.read_csv('train.csv')
-#with open('reference_file.yaml') as f:
-    #column_dict = yml.safe_load(f)
+
+def load_yml():
+    with open('reference_file.yml', 'r') as f:
+       column_dict = yml.load(f)
+    return column_dict
+
+def cat_var_transform(df, cat_dict):
+    remove_items = ['categorical', 'non-categorical']
+    for key in cat_dict:
+        if key not in remove_items:
+            print(cat_dict[key])
+            df[key] = df[key].map(lambda x: cat_dict[key][x])
+    return df
+
+df_train = pd.read_csv('train.csv', keep_default_na=False)
+cat_dict = load_yml()
+df_train = cat_var_transform(df_train, cat_dict)
+df_train = df_train[[i for i in cat_dict if i not in ['categorical', 'non-categorical']]]
+df_train.to_csv('test.csv')
 
