9d9f7b1dfed276e87c6840cb205474d19f62e428
DataMonsterBoy <d.j.g.wyatt@gmail.com>
2017-08-23 10:08:20 +0100

    missing data

--- code/cat_variables.py
+++ code/cat_variables.py
@@ -11,13 +11,6 @@ def cat_var_transform(df, cat_dict):
     remove_items = ['categorical', 'non-categorical']
     for key in cat_dict:
         if key not in remove_items:
-            print(cat_dict[key])
             df[key] = df[key].map(lambda x: cat_dict[key][x])
     return df
-
-df_train = pd.read_csv('train.csv', keep_default_na=False)
-cat_dict = load_yml()
-df_train = cat_var_transform(df_train, cat_dict)
-df_train = df_train[[i for i in cat_dict if i not in ['categorical', 'non-categorical']]]
-df_train.to_csv('test.csv')
 
